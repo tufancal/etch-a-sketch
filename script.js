2056c4f3e94479f37bbfd4a1bfcb1b2e3d6a75de
twofun2fun <tufan.calisir@outlook.de>
2022-03-10 09:16:43 +0100

    Added the basic 16x16 Grid

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 "use strict";
+
+// VARIABLE DECLARATION
+const container = document.getElementById("container");
+
+// Create a 16x16 grid
+
+function createDiv() {
+  const divSquare = document.createElement("div");
+  divSquare.className = "square16";
+  return divSquare;
+}
+
+function divsInArray() {
+  let myDivs = [];
+  let numberOfSquares = 16;
+
+  for (let i = 0; i < numberOfSquares; i++) {
+    myDivs.push(createDiv());
+    container.appendChild(myDivs[i]);
+  }
+}
+
+divsInArray();
