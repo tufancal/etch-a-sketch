aafb9dcc42b63e8128e22e1da2f3d0289ee87d50
twofun2fun <tufan.calisir@outlook.de>
2022-03-10 15:36:15 +0100

    Added basic functions of buttons: draw, erase, reset

--- script.js
+++ script.js
@@ -2,8 +2,11 @@
 
 // VARIABLE DECLARATION
 const container = document.getElementById("container");
+const reset = document.getElementById("reset");
+const erase = document.getElementById("erase");
+const draw = document.getElementById("draw");
 let myDivs = [];
-let numberOfSquares = 256;
+let numberOfSquares16 = 256;
 
 // Create a 16x16 grid
 function createDiv() {
@@ -13,7 +16,7 @@ function createDiv() {
 }
 
 function divsInArray() {
-  for (let i = 0; i < numberOfSquares; i++) {
+  for (let i = 0; i < numberOfSquares16; i++) {
     myDivs.push(createDiv());
     container.appendChild(myDivs[i]);
   }
@@ -23,4 +26,28 @@ divsInArray();
 // drawing
 container.addEventListener("mouseover", function (e) {
   e.target.classList.add("drawing");
+});
+
+// reset button
+reset.addEventListener("click", function () {
+  for (let i = 0; i < numberOfSquares16; i++) {
+    myDivs[i].classList.remove("drawing");
+  }
+  container.addEventListener("mouseover", function (e) {
+    e.target.classList.add("drawing");
+  });
+});
+
+// draw button
+draw.addEventListener("click", function () {
+  container.addEventListener("mouseover", function (e) {
+    e.target.classList.add("drawing");
+  });
+});
+
+// erase button
+erase.addEventListener("click", function () {
+  container.addEventListener("mouseover", function (e) {
+    e.target.classList.remove("drawing");
+  });
 });
